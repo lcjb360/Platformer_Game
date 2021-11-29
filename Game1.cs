0589eb38fb193e410c501bd3cd692d062c58b1e7
Liam Bye <liamcjbye@gmail.com>
2021-11-29 12:21:14 +0000

    moving disappearing platforms

--- Game1.cs
+++ Game1.cs
@@ -36,6 +36,7 @@ namespace Platformer_Game
         public Level two_two;
         public Level three_two;
         public Level four_two;
+        public Level five_two;
 
 
         public Game1()
@@ -223,6 +224,18 @@ namespace Platformer_Game
                 new List<Spike>() { },
                 new List<Lava>() { },
                 SpriteSheet);
+
+            five_two = new Level(true, false, level_number, window, new Vector2(window.Width - 50, window.Height - 50),
+                new Player(SpriteSheet, new Vector2(30, window.Height - 90), 200),
+                new List<Platform>() { new Platform(SpriteSheet, new Vector2(0, window.Height - 30), window.Width, 30),
+                                       new Platform(SpriteSheet, new Vector2(110,100), 100, 30, true, new Vector2(110,500), true,false,true),
+                                     },
+                new List<Wall>() {     new Wall(SpriteSheet, new Vector2(0, 0), 30, window.Height),
+                                       new Wall(SpriteSheet, new Vector2(window.Width-30, 0), 30, window.Height),
+                                       new Wall(SpriteSheet, new Vector2(0, 0), window.Width, 30)},
+                new List<Spike>() { },
+                new List<Lava>() { },
+                SpriteSheet);
         }
 
         protected override void Update(GameTime gameTime)
@@ -348,7 +361,21 @@ namespace Platformer_Game
                         if (four_two.Update(gameTime))
                         {
                             four_two.Completed = true;
-                            //five_two.Unlocked = true;
+                            five_two.Unlocked = true;
+                            game_state = "5,2";
+                        }
+                    }
+                    else
+                    {
+                        game_state = "Main_Menu";
+                    }
+                    break;
+                case "5,2":
+                    if (five_two.Unlocked)
+                    {
+                        if (five_two.Update(gameTime))
+                        {
+                            five_two.Completed = true;
                             game_state = "Main_Menu";
                         }
                     }
@@ -430,6 +457,13 @@ namespace Platformer_Game
                     {
                         GraphicsDevice.Clear(new Color(100, 200, 75));
                         four_two.Draw(_spriteBatch, gameTime, SpriteSheet);
+                    }
+                    break;
+                case "5,2":
+                    if (five_two.Unlocked)
+                    {
+                        GraphicsDevice.Clear(new Color(100, 200, 75));
+                        five_two.Draw(_spriteBatch, gameTime, SpriteSheet);
                     }
                     break;
                 case ("Main_Menu"):
