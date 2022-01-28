2bac5c5377c63779b45b29782ca51839fb776c9d
Liam <liamcjbye@gmail.com>
2022-01-28 15:07:30 +0000

    OOPed

--- Lava.cs
+++ Lava.cs
@@ -6,42 +6,36 @@ using System.Text;
 
 namespace Platformer_Game
 {
-    public class Lava
+    public class Lava : Visible_Object
     {
-        public Rectangle window = new Rectangle(0, 0, GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width, GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height);
-        
-        public Sprite Default_Lava;
-        public Vector2 Position;
         public int Length;
-        public int Width = 60;
-        public int Height = 12;
 
         public Lava(Texture2D texture, Vector2 position, int length)
         {
             float w_ratio = 1;
             float h_ratio = 1;
-            Default_Lava = new Sprite(texture, 0, 76, Width, Height);
+            Default_Sprite = new Sprite(texture, 0, 76, 60, 12);
             Position = new Vector2(position.X * w_ratio, position.Y * h_ratio);
             Length = (int)((float)length * w_ratio);
-            Width = (int)((float)Width * w_ratio);
-            Height = (int)((float)Height * h_ratio);
+            Width = 60;
+            Height = 12;
         }
         public Lava(Texture2D texture, Vector2 position, int length, int height)
         {
             float w_ratio = 1;
             float h_ratio = 1;
-            Default_Lava = new Sprite(texture, 0, 76, Width, Height);
+            Default_Sprite = new Sprite(texture, 0, 76, 60, 12);
             Position = new Vector2(position.X * w_ratio, position.Y * h_ratio);
             Length = (int)((float)length * w_ratio);
-            Width = (int)((float)Width * w_ratio);
-            Height = (int)((float)height * h_ratio);
+            Width = 60;
+            Height = height;
         }
 
         public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
-            for (int x = (int)Position.X; x < ((int)Position.X + Length); x += Width)
+            for (int x = (int)Position.X; x < ((int)Position.X + Length); x += (int)Width)
             {
-                Default_Lava.Draw(spriteBatch, new Vector2(x, Position.Y), Width, Height);
+                Default_Sprite.Draw(spriteBatch, new Vector2(x, Position.Y), (int)Width, (int)Height);
             }
         }
     }
