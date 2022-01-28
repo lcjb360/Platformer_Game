b2528d4b4644ce1609370ee86c1a677466c16338
Liam <liamcjbye@gmail.com>
2022-01-28 15:07:30 +0000

    OOPed

---
+++ Physics_Object.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Platformer_Game
+{
+    public class Physics_Object : Visible_Object
+    {
+        public Vector2 Velocity;
+    }
+}
